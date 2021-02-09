fa9d0614dd588fcf36eae47be9fa83d515ab9435
denismalovany <denismalovany@gmail.com>
2021-02-10 01:10:57 +0200

    list of teachers

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,6 +61,7 @@ module.exports = {
         'hover': '0px 10px 20px rgba(0, 0, 0, 0.04), 0px 2px 6px rgba(0, 0, 0, 0.04)',
         'medium': '0px 10px 20px rgba(0, 0, 0, 0.04), 0px 2px 6px rgba(0, 0, 0, 0.04)',
         'large': '0px 16px 24px rgba(0, 0, 0, 0.06), 0px 2px 6px rgba(0, 0, 0, 0.04)',
+        'base': '0px 4px 8px rgba(0, 0, 0, 0.04), 0px 0px 2px rgba(0, 0, 0, 0.06)',
       },
       backgroundImage: {
         'videocard': "url('img/pretty-asian-teacher-smiling-camera-back-classroom-elementary-school-vintage-effect-style-pictures 1.jpg')",
